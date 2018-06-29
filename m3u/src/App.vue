0126214293ebf6c90060b36cd26f2b2fd6df9b12
Anton <anton@chernianu.com>
2018-06-30 01:03:52 +0300

    start project

---
+++ m3u/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="app">
+
+        <player></player>
+        <!--<router-view/>-->
+    </div>
+</template>
+
+<script>
+    import player from '@/components/common/player';
+
+    export default {
+        name: 'App',
+        components: {
+            player
+        }
+    }
+</script>
+
+<style>
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+</style>
---
+++ m3u/src/components/common/player.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="player">
+        <video ref="playerM" id="playerMMM" width=600 height=300 class="video-js vjs-default-skin" controls>
+            <source
+                    src="http://cdnworld.magictvbox.eu/Discovery_Showcase_HD/tracks-v2a1/index.m3u8?token=b04f6e1d3917019e1ca2559684a5aad5673f376e"
+                    type="application/x-mpegURL">
+        </video>
+    </div>
+</template>
+
+<script>
+    import videojs from 'video.js';
+    import videoPlayer from 'videojs-contrib-hls';
+
+    export default {
+        name: 'player',
+        components: {
+        },
+        data() {
+            return{
+            }
+        },
+        mounted() {
+            let playerID = document.querySelector('#playerMMM');
+            let playerElem = videojs(playerID, {html5: {
+                hls: {
+                    withCredentials: false
+                }
+            }});
+            playerElem.play();
+
+        }
+
+    }
+</script>
